b2e8cf3566cf0fbe0c0c6e7540b9ed871e7eecc2
Jon-Felipe <rodriguesjf7@gmail.com>
2023-02-11 18:34:56 +0000

    Added page navigation links

--- src/components/header/PageLinks.jsx
+++ src/components/header/PageLinks.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+
+// links
+import { pageLinks } from '../../utils/constants';
 
 const PageLinks = () => {
-  return <div>PageLinks</div>;
+  return (
+    <PageLinksWrapper>
+      {pageLinks.map((link) => {
+        return <li key={link.id}>{link.text}</li>;
+      })}
+    </PageLinksWrapper>
+  );
 };
 
 export default PageLinks;
+
+const PageLinksWrapper = styled.ul`
+  display: flex;
+  align-items: center;
+`;
