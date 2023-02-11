f911855ab1993f2bbd5ac95e403466dec4f88a3f
Jon-Felipe <rodriguesjf7@gmail.com>
2023-02-11 19:02:25 +0000

    Profile links styling

--- src/components/header/ProfileLinks.jsx
+++ src/components/header/ProfileLinks.jsx
@@ -16,4 +16,24 @@ const ProfileLinks = () => {
 
 export default ProfileLinks;
 
-const ProfileLinksWrapper = styled.ul``;
+const ProfileLinksWrapper = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  column-gap: 0.5rem;
+  li {
+    border: 1px solid var(--primary-500);
+    border-radius: var(--borderRadius);
+    background-color: var(--primary-500);
+    color: var(--white);
+    padding: 5px 15px;
+    font-size: 1.15rem;
+    letter-spacing: var(--letterSpacing);
+    cursor: pointer;
+  }
+  li:hover {
+    background-color: var(--white);
+    color: var(--primary-500);
+    transition: var(--transition);
+  }
+`;
