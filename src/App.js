60251ff29d1ddcbd4f4d22bb69bf555c659796cb
Jon-Felipe <rodriguesjf7@gmail.com>
2023-02-11 15:15:13 +0000

    Created Header and Footer components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+// components
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
 function App() {
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
+    <>
+      <Header />
+      <main>
+        <h1>hello world</h1>
+      </main>
+      <Footer />
+    </>
   );
 }
 
