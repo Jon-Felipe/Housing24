f4eed98ac0428e27c790578df531fcbe02ef5865
Jon-Felipe <rodriguesjf7@gmail.com>
2023-02-11 15:40:39 +0000

    Styled Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // components
 import NavLinks from './NavLinks';
@@ -6,11 +7,56 @@ import ProfileLinks from './ProfileLinks';
 
 const Header = () => {
   return (
-    <nav>
-      <NavLinks />
-      <ProfileLinks />
-    </nav>
+    <HeaderWrapper>
+      <div className='header-content'>
+        <div className='header-actions'>
+          <h1>Housing24</h1>
+          <div className='toggle-btn'>
+            <p>x</p>
+          </div>
+        </div>
+        <nav className='links'>
+          <NavLinks />
+          <ProfileLinks />
+        </nav>
+      </div>
+    </HeaderWrapper>
   );
 };
 
 export default Header;
+
+const HeaderWrapper = styled.header`
+  height: 5rem;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+  display: flex;
+  align-items: center;
+  .header-content {
+    width: 90vw;
+    max-width: 1440px;
+    margin: 0 auto;
+  }
+  .header-actions {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .links {
+    display: none;
+  }
+
+  @media screen and (min-width: 670px) {
+    .toggle-btn {
+      display: none;
+    }
+    .header-content {
+      display: grid;
+      grid-template-columns: 200px 1fr;
+      place-items: center;
+    }
+    .links {
+      display: flex;
+      align-items: center;
+    }
+  }
+`;
